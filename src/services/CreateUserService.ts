25f7d2b920f625399981de4ffd7fca079d24f591
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2020-07-27 16:34:12 -0300

    Add user create

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import User from '../models/User';
+import { hash } from 'bcryptjs';
 
 interface Request {
   name: string;
@@ -18,14 +19,16 @@ class CreateUserService {
       throw new Error('Email address already used.');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
-
+    delete user.password;
     return user;
   }
 }
